6dbf33f36036a90623b66240e1aa3b19322a6cb2
wanbo.ge <wanbo.ge@condenast.com.cn>
2020-04-21 19:09:59 +0800

    update

--- ocr/Chinese_OCR.py
+++ ocr/Chinese_OCR.py
@@ -1,10 +1,6 @@
-﻿#! /usr/bin/env python
-# -*- coding: utf-8 -*-
-
-# top 1 accuracy 0.99826 top 5 accuracy 0.99989
-
+﻿import importlib
 import os
-os.environ["CUDA_VISIBLE_DEVICES"] = "4"
+# os.environ["CUDA_VISIBLE_DEVICES"] = "4"
 import random
 import tensorflow.contrib.slim as slim
 import time
@@ -16,8 +12,9 @@ from PIL import Image
 import cv2
 from tensorflow.python.ops import control_flow_ops
 import sys
-reload(sys)
-sys.setdefaultencoding('utf-8')
+
+importlib.reload(sys)
+# sys.setdefaultencoding('utf-8')
 
 logger = logging.getLogger('Training a chinese write char recognition')
 logger.setLevel(logging.INFO)
@@ -41,14 +38,14 @@ tf.app.flags.DEFINE_integer('save_steps', 500, "the steps to save")
 tf.app.flags.DEFINE_string('checkpoint_dir', './checkpoint/', 'the checkpoint dir')
 tf.app.flags.DEFINE_string('train_data_dir', './dataset/train/', 'the train dataset dir')
 tf.app.flags.DEFINE_string('test_data_dir', './dataset/test/', 'the test dataset dir')
-tf.app.flags.DEFINE_string('log_dir', './log', 'the logging dir')
+tf.app.flags.DEFINE_string('app_log_dir', './log', 'the logging dir')
 
 tf.app.flags.DEFINE_boolean('restore', False, 'whether to restore from checkpoint')
 tf.app.flags.DEFINE_boolean('epoch', 1, 'Number of epoches')
 tf.app.flags.DEFINE_integer('batch_size', 128, 'Validation batch size')
-tf.app.flags.DEFINE_string('mode', 'validation', 'Running mode. One of {"train", "valid", "test"}')
+tf.app.flags.DEFINE_string('mode', 'train', 'Running mode. One of {"train", "valid", "test"}')
 
-gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.333)
+# gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.333)
 FLAGS = tf.app.flags.FLAGS
 
 
@@ -62,7 +59,7 @@ class DataIterator:
         for root, sub_folder, file_list in os.walk(data_dir):
             if root < truncate_path:
                 self.image_names += [os.path.join(root, file_path) for file_path in file_list]
-        random.shuffle(self.image_names) # 打乱
+        random.shuffle(self.image_names)  # 打乱
         # 例如image_name为./train/00001/2.png，提取00001就是其label
         self.labels = [int(file_name[len(data_dir):].split(os.sep)[0]) for file_name in self.image_names]
 
@@ -82,6 +79,7 @@ class DataIterator:
         if FLAGS.random_contrast:
             images = tf.image.random_contrast(images, 0.8, 1.2)
         return images
+
     # batch的生成
     def input_pipeline(self, batch_size, num_epochs=None, aug=False):
         # numpy array 转 tensor
@@ -96,7 +94,7 @@ class DataIterator:
         if aug:
             images = self.data_augmentation(images)
         new_size = tf.constant([FLAGS.image_size, FLAGS.image_size], dtype=tf.int32)
-        images = tf.image.resize_images(images, new_size)
+        images = tf.image.resize(images, new_size)
         image_batch, label_batch = tf.train.shuffle_batch([images, labels], batch_size=batch_size, capacity=50000,
                                                           min_after_dequeue=10000)
         # print 'image_batch', image_batch.get_shape()
@@ -104,14 +102,15 @@ class DataIterator:
 
 
 def build_graph(top_k):
-    keep_prob = tf.placeholder(dtype=tf.float32, shape=[], name='keep_prob') # dropout打开概率
-    images = tf.placeholder(dtype=tf.float32, shape=[None, 64, 64, 1], name='image_batch')
-    labels = tf.placeholder(dtype=tf.int64, shape=[None], name='label_batch')
-    is_training = tf.placeholder(dtype=tf.bool, shape=[], name='train_flag')
-    with tf.device('/gpu:5'):
+    keep_prob = tf.compat.v1.placeholder(dtype=tf.float32, shape=[], name='keep_prob')  # dropout打开概率
+    images = tf.compat.v1.placeholder(dtype=tf.float32, shape=[None, 64, 64, 1], name='image_batch')
+    labels = tf.compat.v1.placeholder(dtype=tf.int64, shape=[None], name='label_batch')
+    is_training = tf.compat.v1.placeholder(dtype=tf.bool, shape=[], name='train_flag')
+    # with tf.device('/gpu:5'):
+    with tf.device('/cpu:0'):
         # network: conv2d->max_pool2d->conv2d->max_pool2d->conv2d->max_pool2d->conv2d->conv2d->
         # max_pool2d->fully_connected->fully_connected
-        #给slim.conv2d和slim.fully_connected准备了默认参数：batch_norm
+        # 给slim.conv2d和slim.fully_connected准备了默认参数：batch_norm
         with slim.arg_scope([slim.conv2d, slim.fully_connected],
                             normalizer_fn=slim.batch_norm,
                             normalizer_params={'is_training': is_training}):
@@ -134,20 +133,20 @@ def build_graph(top_k):
         loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=labels))
         accuracy = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(logits, 1), labels), tf.float32))
 
-        update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+        update_ops = tf.compat.v1.get_collection(tf.compat.v1.GraphKeys.UPDATE_OPS)
         if update_ops:
             updates = tf.group(*update_ops)
             loss = control_flow_ops.with_dependencies([updates], loss)
 
-        global_step = tf.get_variable("step", [], initializer=tf.constant_initializer(0.0), trainable=False)
-        optimizer = tf.train.AdamOptimizer(learning_rate=0.1)
+        global_step = tf.compat.v1.get_variable("step", [], initializer=tf.constant_initializer(0.0), trainable=False)
+        optimizer = tf.compat.v1.train.AdamOptimizer(learning_rate=0.1)
         train_op = slim.learning.create_train_op(loss, optimizer, global_step=global_step)
         probabilities = tf.nn.softmax(logits)
 
         # 绘制loss accuracy曲线
-        tf.summary.scalar('loss', loss)
-        tf.summary.scalar('accuracy', accuracy)
-        merged_summary_op = tf.summary.merge_all()
+        tf.compat.v1.summary.scalar('loss', loss)
+        tf.compat.v1.summary.scalar('accuracy', accuracy)
+        merged_summary_op = tf.compat.v1.summary.merge_all()
         # 返回top k 个预测结果及其概率；返回top K accuracy
         predicted_val_top_k, predicted_index_top_k = tf.nn.top_k(probabilities, k=top_k)
         accuracy_in_top_k = tf.reduce_mean(tf.cast(tf.nn.in_top_k(probabilities, labels, top_k), tf.float32))
@@ -170,23 +169,26 @@ def build_graph(top_k):
 
 def train():
     print('Begin training')
+    print('Log dir:')
+    print(FLAGS.app_log_dir)
     # 填好数据读取的路径
     train_feeder = DataIterator(data_dir='./dataset/train/')
     test_feeder = DataIterator(data_dir='./dataset/test/')
     model_name = 'chinese-rec-model'
-    with tf.Session(config=tf.ConfigProto(gpu_options=gpu_options, allow_soft_placement=True)) as sess:
+    # with tf.compat.v1.Session(config=tf.compat.v1.ConfigProto(gpu_options=gpu_options, allow_soft_placement=True)) as sess:
+    with tf.compat.v1.Session(config=tf.compat.v1.ConfigProto(allow_soft_placement=True)) as sess:
         # batch data 获取
         train_images, train_labels = train_feeder.input_pipeline(batch_size=FLAGS.batch_size, aug=True)
         test_images, test_labels = test_feeder.input_pipeline(batch_size=FLAGS.batch_size)
         graph = build_graph(top_k=1)  # 训练时top k = 1
-        saver = tf.train.Saver()
-        sess.run(tf.global_variables_initializer())
+        saver = tf.compat.v1.train.Saver()
+        sess.run(tf.compat.v1.global_variables_initializer())
         # 设置多线程协调器
         coord = tf.train.Coordinator()
         threads = tf.train.start_queue_runners(sess=sess, coord=coord)
 
-        train_writer = tf.summary.FileWriter(FLAGS.log_dir + '/train', sess.graph)
-        test_writer = tf.summary.FileWriter(FLAGS.log_dir + '/val')
+        train_writer = tf.compat.v1.summary.FileWriter(FLAGS.app_log_dir + '/train', sess.graph)
+        test_writer = tf.compat.v1.summary.FileWriter(FLAGS.app_log_dir + '/val')
         start_step = 0
         # 可以从某个step下的模型继续训练
         if FLAGS.restore:
@@ -250,12 +252,13 @@ def validation():
     final_predict_index = []
     groundtruth = []
 
-    with tf.Session(config=tf.ConfigProto(gpu_options=gpu_options,allow_soft_placement=True)) as sess:
+    # with tf.compat.v1.Session(config=tf.compat.v1.ConfigProto(gpu_options=gpu_options,allow_soft_placement=True)) as sess:
+    with tf.compat.v1.Session(config=tf.compat.v1.ConfigProto(allow_soft_placement=True)) as sess:
         test_images, test_labels = test_feeder.input_pipeline(batch_size=FLAGS.batch_size, num_epochs=1)
         graph = build_graph(top_k=5)
-        saver = tf.train.Saver()
+        saver = tf.compat.v1.train.Saver()
 
-        sess.run(tf.global_variables_initializer())
+        sess.run(tf.compat.v1.global_variables_initializer())
         sess.run(tf.local_variables_initializer())  # initialize test_feeder's inside state
 
         coord = tf.train.Coordinator()
@@ -302,9 +305,10 @@ def validation():
         coord.join(threads)
     return {'prob': final_predict_val, 'indices': final_predict_index, 'groundtruth': groundtruth}
 
+
 # 获待预测图像文件夹内的图像名字
 def get_file_list(path):
-    list_name=[]
+    list_name = []
     files = os.listdir(path)
     files.sort()
     for file in files:
@@ -312,27 +316,30 @@ def get_file_list(path):
         list_name.append(file_path)
     return list_name
 
+
 # 图像二值化，需注意待预测的汉字是黑底白字还是白底黑字
 def binary_pic(name_list):
     for image in name_list:
         temp_image = cv2.imread(image)
-        #print image
-        GrayImage=cv2.cvtColor(temp_image,cv2.COLOR_BGR2GRAY) 
-        ret,thresh1=cv2.threshold(GrayImage,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+        # print image
+        GrayImage = cv2.cvtColor(temp_image, cv2.COLOR_BGR2GRAY)
+        ret, thresh1 = cv2.threshold(GrayImage, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
         single_name = image.split('t/')[1]
-        print single_name
-        cv2.imwrite('../data/tmp/'+single_name,thresh1)
+        print(single_name)
+        cv2.imwrite('../data/tmp/' + single_name, thresh1)
+
 
 # 获取汉字label映射表
 def get_label_dict():
-    f=open('./chinese_labels','r')
+    f = open('./chinese_labels', 'rb')
     label_dict = pickle.load(f)
     f.close()
     return label_dict
 
+
 def inference(name_list):
     print('inference')
-    image_set=[]
+    image_set = []
     # 对每张图进行尺寸标准化和归一化
     for image in name_list:
         temp_image = Image.open(image).convert('L')
@@ -340,35 +347,38 @@ def inference(name_list):
         temp_image = np.asarray(temp_image) / 255.0
         temp_image = temp_image.reshape([-1, 64, 64, 1])
         image_set.append(temp_image)
-        
+
     # allow_soft_placement 如果你指定的设备不存在，允许TF自动分配设备
-    with tf.Session(config=tf.ConfigProto(gpu_options=gpu_options,allow_soft_placement=True)) as sess:
+    # with tf.compat.v1.Session(config=tf.compat.v1.ConfigProto(gpu_options=gpu_options,allow_soft_placement=True)) as sess:
+    with tf.compat.v1.Session(config=tf.compat.v1.ConfigProto(allow_soft_placement=True)) as sess:
         logger.info('========start inference============')
-        # images = tf.placeholder(dtype=tf.float32, shape=[None, 64, 64, 1])
+        # images = tf.compat.v1.placeholder(dtype=tf.float32, shape=[None, 64, 64, 1])
         # Pass a shadow label 0. This label will not affect the computation graph.
         graph = build_graph(top_k=3)
-        saver = tf.train.Saver()
+        saver = tf.compat.v1.train.Saver()
         # 自动获取最后一次保存的模型
         ckpt = tf.train.latest_checkpoint(FLAGS.checkpoint_dir)
-        if ckpt:       
+        if ckpt:
             saver.restore(sess, ckpt)
-        val_list=[]
-        idx_list=[]
+        val_list = []
+        idx_list = []
         # 预测每一张图
         for item in image_set:
             temp_image = item
             predict_val, predict_index = sess.run([graph['predicted_val_top_k'], graph['predicted_index_top_k']],
-                                              feed_dict={graph['images']: temp_image,
-                                                         graph['keep_prob']: 1.0,
-                                                         graph['is_training']: False})
+                                                  feed_dict={graph['images']: temp_image,
+                                                             graph['keep_prob']: 1.0,
+                                                             graph['is_training']: False})
             val_list.append(predict_val)
             idx_list.append(predict_index)
-    #return predict_val, predict_index
-    return val_list,idx_list
+    # return predict_val, predict_index
+    return val_list, idx_list
 
 
 def main(_):
+    print('---------')
     print(FLAGS.mode)
+
     if FLAGS.mode == "train":
         train()
     elif FLAGS.mode == 'validation':
@@ -381,23 +391,38 @@ def main(_):
     elif FLAGS.mode == 'inference':
         label_dict = get_label_dict()
         name_list = get_file_list('./tmp')
-        #binary_pic(name_list)
-        #tmp_name_list = get_file_list('../data/tmp')
+        # binary_pic(name_list)
+        # tmp_name_list = get_file_list('../data/tmp')
         # 将待预测的图片名字列表送入predict()进行预测，得到预测的结果及其index
         final_predict_val, final_predict_index = inference(name_list)
-        final_reco_text =[]  # 存储最后识别出来的文字串
+        final_reco_text = []  # 存储最后识别出来的文字串
         # 给出top 3预测，candidate1是概率最高的预测
         for i in range(len(final_predict_val)):
             candidate1 = final_predict_index[i][0][0]
             candidate2 = final_predict_index[i][0][1]
             candidate3 = final_predict_index[i][0][2]
             final_reco_text.append(label_dict[int(candidate1)])
-            logger.info('[the result info] image: {0} predict: {1} {2} {3}; predict index {4} predict_val {5}'.format(name_list[i], 
-                label_dict[int(candidate1)],label_dict[int(candidate2)],label_dict[int(candidate3)],final_predict_index[i],final_predict_val[i]))
-        print ('=====================OCR RESULT=======================\n')
+            logger.info('[the result info] image: {0} predict: {1} {2} {3}; predict index {4} predict_val {5}'.format(
+                name_list[i],
+                label_dict[int(candidate1)], label_dict[int(candidate2)], label_dict[int(candidate3)],
+                final_predict_index[i], final_predict_val[i]))
+        print('=====================OCR RESULT=======================\n')
         # 打印出所有识别出来的结果（取top 1）
         for i in range(len(final_reco_text)):
-           print final_reco_text[i], 
+            print(final_reco_text[i])
+
 
 if __name__ == "__main__":
-    tf.app.run()
+    """
+    Training model:
+    python Chinese_OCR.py --mode=train --max_steps=16002 --eval_steps=100 --save_steps=500
+    
+    Model validation:
+    python Chinese_OCR.py --mode=validation
+    
+    
+    python Chinese_OCR.py --mode=inference
+    
+    """
+    # tf.app.run()
+    tf.compat.v1.app.run()
